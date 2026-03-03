4ff3ce8d7a5288e937f78e210b57062af9242b83
Alex Poirier <poirier.alex2005@gmail.com>
2026-03-03 18:22:52 +0100

    modif fonction scroll

--- src/components/ScrollToHash.jsx
+++ src/components/ScrollToHash.jsx
@@ -2,16 +2,22 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 function ScrollToHash() {
-    const { hash } = useLocation();
+    const { hash, pathname } = useLocation();
 
     useEffect(() => {
         if (hash) {
-            const element = document.querySelector(hash);
+            const selector = hash.startsWith("#") ? hash : `#${hash}`
+            const element = document.querySelector(selector)
             if (element) {
-                element.scrollIntoView({ behavior: "smooth" });
+                const header = document.querySelector('.header'); 
+                const headerHeight = header ? header.getBoundingClientRect().height : 0
+                const top = element.getBoundingClientRect().top + window.pageYOffset - headerHeight;
+                window.scrollTo({ top, behavior: "smooth" })
             }
+        } else {
+            window.scrollTo({ top: 0, behavior: "instant" })
         }
-    }, [hash]);
+    }, [hash, pathname]);
 
     return null;
 }
